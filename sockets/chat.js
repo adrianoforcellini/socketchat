90cdff6a07392b35f6a4feb5f2b121f88d5bd919
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-11 18:23:38 -0300

    Formataçao basica do chat com salas privadas

---
+++ sockets/chat.js
@@ -0,0 +1,7 @@
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.emit('serverMessage', 'Olá, seja bem vindo ao nosso chat público! Use essa página para conversar a vontade.');
+  socket.on('clientMessage', (message) => {
+    console.log(`Mensagem ${message}`);
+    io.emit('serverMessage', message);
+  });
+});
