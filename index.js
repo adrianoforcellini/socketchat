f88566d562936c16031512848263fee867bd147e
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-11 19:57:09 -0300

    express funcionando, testando o funcionamento de get basico

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const app = express();
 
 
 // app.use(express.static(__dirname + '/public'));
-// app.get('/', (_req, res) => {
-// res.send('tacalhepau')
-// })
+app.get('/', (_req, res) => {
+res.send('tacalhepau')
+})
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
