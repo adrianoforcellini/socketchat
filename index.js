a55d3f46a8fb31b0dbf6dd50bbe2e09e89cfb7b1
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-11 19:46:12 -0300

    testando o funcionamento sem socket

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'https://main.d1pdv4im8oqf39.amplifyapp.com/', 
-    methods: ['GET', 'POST'], 
-  },
-});
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: 'https://main.d1pdv4im8oqf39.amplifyapp.com/', 
+//     methods: ['GET', 'POST'], 
+//   },
+// });
 
 
-require('./sockets/chat')(io);
-require('./sockets/rooms')(io);
+// require('./sockets/chat')(io);
+// require('./sockets/rooms')(io);
 
 
 app.use(express.static(__dirname + '/public'));
+app.get('/', (_req, res) => {
+res.send('tacalhepau')
+})
 
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+//   res.redirect('/entrar.html')
+// });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-  res.redirect('/entrar.html')
-});
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
   console.log('The Peoople Is On the Table!!!');
