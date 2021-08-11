32b4a1ac2c3170738e9c4910ee882adafdcf9093
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-11 19:59:48 -0300

    testando http

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const http = require('http').createServer(app);
+const http = require('http').createServer(app);
 
 // const io = require('socket.io')(http, {
 //   cors: {
@@ -30,7 +30,6 @@ const PORT = process.env.PORT || 3000
 //   console.log('The Peoople Is On the Table!!!');
 // });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('The Peoople Is On the Table!!!');
 });
-// test deploy
